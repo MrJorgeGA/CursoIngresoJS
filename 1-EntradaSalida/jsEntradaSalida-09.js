5a4aa53c768a151f531e4c0823ce984e592768b9
MrJorgeGA <jorgegarciaalurralde@gmail.com>
2021-01-30 16:52:57 -0300

    Pequeña correción del punto flotante

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,6 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-	let sueldo = parseInt(document.getElementById('txtIdSueldo').value);
-	document.getElementById('txtIdResultado').value = sueldo*1.1;
+	let sueldo = parseFloat(document.getElementById('txtIdSueldo').value);
+	document.getElementById('txtIdResultado').value = sueldo*110/100 ;
 }
