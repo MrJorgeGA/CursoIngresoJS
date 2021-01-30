ae9fe3a1163dd5c229dd8030369f523a9f6a1f05
MrJorgeGA <jorgegarciaalurralde@gmail.com>
2021-01-30 00:03:02 -0300

    Resolución EyS 3 y 4

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,9 @@
-/* 
-Debemos lograr tomar un dato por 'PROMPT' 
+/*
+Debemos lograr tomar un dato por 'PROMPT'
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombreIngresado = prompt("Ingrese su nombre");
+	txtIdNombre.value = nombreIngresado;//asigno valor del "nombreIngresado" al cuadro de texto, lue
 }
-
