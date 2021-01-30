2bc33660dd66cc19f704ca57c3122e3bba25dca7
MrJorgeGA <jorgegarciaalurralde@gmail.com>
2021-01-29 23:48:30 -0300

    Resolucion EyS 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,8 @@
 /*
-Debemos lograr tomar un nombre con 'prompt' 
+Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let	mensaje = prompt('Escriba su nombre');
+	alert(mensaje);
 }
-
