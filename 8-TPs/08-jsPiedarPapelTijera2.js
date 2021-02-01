2fc11ae92390ad352d7cc3b69d3b78d0cc85d108
MrJorgeGA <jorgegarciaalurralde@gmail.com>
2021-02-01 11:40:15 -0300

    TP 8 conmodificación al html para que funcione correctamente el programa solicitado :)

--- 8-TPs/08-jsPiedarPapelTijera2.js
+++ 8-TPs/08-jsPiedarPapelTijera2.js
@@ -1,31 +1,50 @@
-var eleccionMaquina;
-var ContadorDeEmpates=0;
-var ContadorDeGanadas=0;
-var ContadorDePerdidas=0;
+let eleccionMaquina;
+let ContadorDeEmpates=0;
+let ContadorDeGanadas=0;
+let ContadorDePerdidas=0;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 3
-	 	
-
-
-
-}//FIN DE LA FUNCIÓN
+	//Formula mágica para número aleatorio
+	let maximo = 3;
+	let minimo = 1;
+	random = Math.round(Math.random()*(maximo-minimo)+minimo);
+	//segun resultado asignamos la eleccion en el jue
+	switch (random) {
+		case 1: eleccionMaquina = "piedra"; break;
+		case 2: eleccionMaquina = "papel"; break;
+		case 3: eleccionMaquina = "tijera";break;
+	};
+}
 function piedra()
 {
-	
-
-}//FIN DE LA FUNCIÓN
+	//Según elección determinamos al ganador siguiendo las reglas y sumamos al contador correspondiente.
+	switch (eleccionMaquina) {
+		case "piedra": alert("EMPATE"); ContadorDeEmpates++; break;
+		case "papel": alert("Usted PIERDE"); ContadorDePerdidas++; break;
+		case "tijera": alert("Usted GANA"); ContadorDeGanadas++; break;
+	};
+}
 function papel()
 {
-
-}//FIN DE LA FUNCIÓN
+	switch (eleccionMaquina) {
+		case "piedra": alert("Usted GANA"); ContadorDeGanadas++; break;
+		case "papel": alert("EMPATE"); ContadorDeEmpates++; break;
+		case "tijera": alert("Usted PIERDE"); ContadorDePerdidas++; break;
+	};
+}
 function tijera()
 {
-	
-}//FIN DE LA FUNCIÓN
+	switch (eleccionMaquina) {
+		case "piedra": alert("Usted PIERDE"); ContadorDePerdidas++; break;
+		case "papel": alert("Usted GANA"); ContadorDeGanadas++; break;
+		case "tijera": alert("EMPATE"); ContadorDeEmpates++; break;
+	};
+}
 
 function mostrarResultado()
 {
-
+	document.getElementById('txtIdGanadas').value = "Juegos ganados: " + ContadorDeGanadas;
+	document.getElementById('txtIdPerdidas').value = "Juegos perdidos: " + ContadorDePerdidas;
+	document.getElementById('txtIdEmpatadas').value = "Juegos empatados: " + ContadorDeEmpates;
 }
