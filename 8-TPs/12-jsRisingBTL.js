610c5f63520462b3c07fa71328c1e86497f03f50
MrJorgeGA <jorgegarciaalurralde@gmail.com>
2021-02-06 23:10:18 -0300

    Tps bucles

--- 8-TPs/12-jsRisingBTL.js
+++ 8-TPs/12-jsRisingBTL.js
@@ -1,4 +1,4 @@
-/*RISING BTL. Empresa dedicada a la toma de datos para realizar estadísticas y censos nos pide realizar una carga de datos validada e ingresada por ventanas emergentes solamente (para evitar hacking y cargas maliciosas) y luego asignarla a cuadros de textos. 
+/*RISING BTL. Empresa dedicada a la toma de datos para realizar estadísticas y censos nos pide realizar una carga de datos validada e ingresada por ventanas emergentes solamente (para evitar hacking y cargas maliciosas) y luego asignarla a cuadros de textos.
 12.   Los datos requeridos son los siguientes:
 A.	Edad, entre 18 y 90 años inclusive.
 B.	Sexo, “M” para masculino y “F” para femenino
@@ -7,13 +7,104 @@ D.	Sueldo bruto, no menor a 8000.
 E.	Número de legajo, numérico de 4 cifras, sin ceros a la izquierda.
 F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.
  */
-function ComenzarIngreso () 
+function ComenzarIngreso ()
 {
  	//definicion de variables
- 	var edadIngresada;
- 	var sexoIngresado;
- 	var estadoCivilIngresado;
-
-
-
+ 	let edadIngresada;
+ 	let sexoIngresado;
+ 	let estadoCivilIngresado;
+  let sueldoBruto;
+  let legajo;
+  let nacionalidad;
+  //dowhile hasta edad válida
+  do {
+    edadIngresada=parseInt(prompt("Ingrese una edad válida (18-90)"));
+  } while (!(edadIngresada<=90&&edadIngresada>=18));
+  //do while sexo valido (qué binario che...)
+  do {
+    sexoIngresado=prompt('Ingrese sexo ("M" o "F")').toUpperCase();
+  } while (sexoIngresado!="M" && sexoIngresado!='F');
+  //do while estado civil
+  do {
+    estadoCivilIngresado=parseInt(prompt('Ingrese estado civil:\n1. Soltero/a\n2. Casado/a\n3. Divorciado/a\n4. Viudo/a'));
+  } while (!(estadoCivilIngresado>=1&&estadoCivilIngresado<=4));
+  //do while sueldo no menor a 8000
+  do {
+    sueldoBruto=parseFloat(prompt('Ingrese sueldo bruto, no menor a $8000'));
+  } while (!(sueldoBruto>=8000));
+  //do while legajo entre 1000 y 9999
+  do {
+    legajo=parseInt(prompt('Ingrese legajo:\n- Debe tener 4 dígitos\n- No debe empezar con "0"'));
+  } while (!(legajo>=1000&&legajo<=9999));
+  //do while nacionalidad
+  do {
+    nacionalidad=prompt('Ingrese nacionalidad\n- Argentinos: "A"\n- Extranjeros: "E"\n- Nacionalizados: "N"').toLowerCase();
+  } while (nacionalidad!="a"&&nacionalidad!="e"&&nacionalidad!="n");
+  //pongo en el textbox la edad validada
+  document.getElementById('txtIdEdad').value='Edad: '+edadIngresada+' años';
+  //segun sexo se llenan en masculino o femeninos los textboxes de sexo estado civil y nacionalidad
+  if (sexoIngresado=="M") {
+    //si es masculino
+    document.getElementById('txtIdSexo').value='Sexo: Masculino';
+    //swith para los distintos estados civiles
+    switch (estadoCivilIngresado) {
+      case 1:
+        document.getElementById('txtIdEstadoCivil').value='Estado Civil: Soltero';
+        break;
+      case 2:
+        document.getElementById('txtIdEstadoCivil').value='Estado Civil: Casado';
+        break;
+      case 3:
+        document.getElementById('txtIdEstadoCivil').value='Estado Civil: Divorciado';
+        break;
+      case 4:
+        document.getElementById('txtIdEstadoCivil').value='Estado Civil: Viudo';
+        break;
+    };
+    //switch para la nacionalidad
+    switch (nacionalidad) {
+      case "a":
+        document.getElementById('txtIdNacionalidad').value='Nacionalidad: Argentino';
+        break;
+      case "e":
+        document.getElementById('txtIdNacionalidad').value='Nacionalidad: Extranjero';
+        break;
+      case "n":
+        document.getElementById('txtIdNacionalidad').value='Nacionalidad: Nacionalizado';
+        break;
+    };
+  } else {
+    //si es femenino
+    document.getElementById('txtIdSexo').value='Sexo: Femenino';
+    //estado civil
+    switch (estadoCivilIngresado) {
+      case 1:
+        document.getElementById('txtIdEstadoCivil').value='Estado Civil: Soltera';
+        break;
+      case 2:
+        document.getElementById('txtIdEstadoCivil').value='Estado Civil: Casada';
+        break;
+      case 3:
+        document.getElementById('txtIdEstadoCivil').value='Estado Civil: Divorciada';
+        break;
+      case 4:
+        document.getElementById('txtIdEstadoCivil').value='Estado Civil: Viuda';
+        break;
+    };
+    //nacionalidad
+    switch (nacionalidad) {
+      case "a":
+        document.getElementById('txtIdNacionalidad').value='Nacionalidad: Argentina';
+        break;
+      case "e":
+        document.getElementById('txtIdNacionalidad').value='Nacionalidad: Extranjera';
+        break;
+      case "n":
+        document.getElementById('txtIdNacionalidad').value='Nacionalidad: Nacionalizada';
+        break;
+    };
+  };
+  //lleno los textboxes de sueldo y de legajo
+  document.getElementById('txtIdSueldo').value='Sueldo Bruto: $'+sueldoBruto;
+  document.getElementById('txtIdLegajo').value="Legajo: "+legajo;
 }
--- 8-TPs/13-jsFonteCristo.js
+++ 8-TPs/13-jsFonteCristo.js
@@ -7,7 +7,122 @@ c.	Se pedirán un número positivo y se mostrará la cantidad de números divisi
 d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
 e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
-function ComenzarIngreso () 
+function NumerosPares()
 {
- 	
+  //inicio el contador
+  let contador = 0;
+  //tomo el número que ingreso el usuario
+  let numero = parseInt(document.getElementById('txtIdNumero').value);
+  //verifivo que este sea un numero natural
+  if (numero>0) {
+    //busco los numeros pares empezando desde el 2 y sumando de a 2 y contabilizo en el contador
+    for (i=2;i<numero;i+=2) {
+      console.log(i);
+      contador++;
+    };
+    //infomro la cantidad que hay en el contador
+    console.log('Hay '+contador+' números pares entre 0 y '+numero);
+  }
+  //el usuario no dio un numero natural
+  else {alert('Entrada Invalida')};
+}
+
+function NumerosImpares()
+{
+  //inicio el contador
+  let contador = 0;
+  //tomo el número que ingreso el usuario
+  let numero = parseInt(document.getElementById('txtIdNumero').value);
+  //verifivo que este sea un numero natural
+  if (numero>0) {
+    //busco los numeros impares empezando desde el 1 y sumando de a 2 y contabilizo en el contador
+    for (i=1;i<numero;i+=2) {
+      console.log(i);
+      contador++;
+    };
+    //infomro la cantidad que hay en el contador
+console.log('Hay '+contador+' números impáres entre 0 y '+numero);
+  }
+  //el usuario no dio un numero natural
+  else {alert('Entrada Invalida')};
+}
+
+function NumerosDivisibles()
+{
+  //inicio el contador
+  let contador = 0;
+  //tomo el número que ingreso el usuario
+  let numero = parseInt(document.getElementById('txtIdNumero').value);
+  //verifivo que este sea un numero natural
+  if (numero>0) {
+    //busco todos los divisores del menores a 100 con el bucle,los informo y contabilizo en elcontador
+    for (i=1;i<101;i++) {
+      if (numero%i==0) {
+        console.log(i);
+        contador++;
+      };
+    };
+    //infomro la cantidad que hay en el contador
+    console.log('Hay '+contador+' divisores de '+numero+' entre 1 y 100');
+  }
+  //el usuario no dio un numero natural
+  else {alert('Entrada Invalida')};
+}
+
+function VerificarPrimo()
+{
+  //inicio el contador
+ 	let contador = 0;
+  //tomo el número que ingreso el usuario
+  let numero = parseInt(document.getElementById('txtIdNumero').value);
+  //verifivo que este sea un numero natural
+  if (numero>0) {
+    //Numero primo: solo dos divisores naturales: 1 y si mismo. Busco cuantos divisores tiene el numero del usuario y los contabilizo con el contador
+    for (i=1;i<=numero;i++) {
+      if (numero%i==0) {
+        console.log(i);
+        contador++;
+      };
+    };
+    //si el contador es 2, tiene 2 divisores, ergo es numero primo, si no, no
+    if (contador==2) {console.log(numero+' es número primo')}
+    else {console.log(numero+' NO es número primo')};
+  }
+  //el usuario no dio un numero natural
+  else {alert('Entrada Invalida')};
+}
+
+function NumerosPrimos()
+{
+  //variables utilizadas
+  let contador;
+  let contadorPrimos = 0;
+  //tomo el número que ingreso el usuario
+  let numero = parseInt(document.getElementById('txtIdNumero').value);
+  //verifivo que este sea un numero natural
+  if (numero>0) {
+    //ciclo for para ir buscando divisores
+    for (i=1; i<=numero;i++) {
+      // si "i" es divisor, inicializo "contador"
+      if (numero%i==0) {
+        contador = 0;
+        //y aplico el bucle para saber si "i" es primo
+        for (b=1;b<=i;b++) {
+          if (i%b==0) {
+            contador++;
+          };
+        };
+        //si lo es, lo cuento en el "contadorPrimos" e informo el numero en cuestion
+        if (contador==2) {
+          contadorPrimos++
+          console.log(i);
+        };
+      };
+    };
+    //dependiendo de la cantidad de divisores primos que tenga el numero del usuario, informo
+    if (contadorPrimos==1) {console.log(numero+' tiene un solo divisor primo');}
+    else {console.log(numero+' tiene '+contadorPrimos+' divisores primos')};
+  }
+  //el usuario no dio un numero natural
+  else {alert('Entrada Invalida')};
 }
